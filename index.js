ae80598cd49ec0185ae89edcf309ce88abf5a7f8
Abdul Sayed <awsayed@alaska.edu>
2020-01-12 19:18:13 -0500

    completed

--- index.js
+++ index.js
@@ -1,14 +1,15 @@
 const express = require('express');
 const app = express();
 const port = 3000;
+var quotes = require('random-movie-quotes')
 
+// Use express server to serve static html file in public folder
 app.use(express.static('public'));
+
+app.listen(port, () => console.log(`Awesome app listening on port ${port}!`));
 
 app.get('/quote', (req, res) => {
   res.json({
     quote: quotes.getQuote()
   });
 });
-
-
-app.listen(port, () => console.log(`Awesome app listening on port ${port}!`));
